c9ce5015772d24cfae7ec5023d6517a4dbfc5943
Antonio Cruz <antonio.cruz5612@gmail.com>
2020-10-29 11:09:31 -0700

    create new repository for SEH coding test. Build MainWindowsControl folder to house logic for api get request. Console.Writeline currently present the parsed json object being pulled from the googlecustomsearch api.

---
+++ SEH_Test_wpf/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using SEH_Test_wpf;
+using Google.Apis.Customsearch.v1;
+using Google.Apis.Customsearch.v1.Data;
+using Google.Apis.Services;
+
+
+namespace SEH_Test_wpf.MainWindowControls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SEH_Test_wpf/MainWindowControls/MainWindowUserControls.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+using SEH_Test_wpf;
+using Google.Apis.Customsearch.v1;
+using Google.Apis.Customsearch.v1.Data;
+using Google.Apis.Services;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace SEH_Test_wpf.MainWindowControls
+{
+    /// <summary>
+    /// Interaction logic for MainWindowUserControls.xaml
+    /// </summary>
+    public partial class MainWindowUserControls : UserControl
+    {
+        public MainWindowUserControls()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            RunAsync();
+            System.Diagnostics.Debug.WriteLine(RunAsync());
+        }
+
+        public string Title { get; set; }
+        public int MaxLength { get; set; }
+
+        static async Task RunAsync()
+        {
+            const string apiKey = "AIzaSyCyqAm432caVHD6ycUEWTbCNtg4rD_ao8Y";
+            const string cx = "f45637bb92df9b998";
+            const string query = "suits";
+            var request = WebRequest.Create("https://www.googleapis.com/customsearch/v1?key=" + apiKey + "&cx=" + cx + "&q=" + query);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseString = reader.ReadToEnd();
+            dynamic jsonData = JsonConvert.DeserializeObject(responseString);
+
+            Console.WriteLine(jsonData);
+        }
+    }
+}
