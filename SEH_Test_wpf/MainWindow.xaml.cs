4d92d1e7335a9d3eb0584f7436374d87f465be3b
Antonio Cruz <antonio.cruz5612@gmail.com>
2021-01-09 16:25:22 -0800

    allocating work done in MainWindowControls to MainWindow.xaml.cs

--- SEH_Test_wpf/MainWindow.xaml.cs
+++ SEH_Test_wpf/MainWindow.xaml.cs
@@ -1,8 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Net;
+using System.Windows;
 using System.Linq;
 using System.Text;
-using System.Windows;
+using System.IO;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System;
 
 
 namespace SEH_Test_wpf.MainWindowControls
@@ -15,7 +20,14 @@ namespace SEH_Test_wpf.MainWindowControls
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+            // The below code shows in the console
+            Console.WriteLine("This is a test");
         }
+
+        public string Title { get; set; }
+        public int MaxLength { get; set; }
+        public string Text { get; internal set; }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
--- SEH_Test_wpf/MainWindowControls/MainWindowUserControls.xaml.cs
+++ SEH_Test_wpf/MainWindowControls/MainWindowUserControls.xaml.cs
@@ -18,39 +18,38 @@ namespace SEH_Test_wpf.MainWindowControls
         {
             InitializeComponent();
             this.DataContext = this;
-            RunAsync();
+            //RunAsync();
             // I am rate limited, but the console log below would show in the output a parsed json object based on the user's query //
-            System.Diagnostics.Debug.WriteLine(RunAsync());
+            //System.Diagnostics.Debug.WriteLine(RunAsync());
             // This is to show that if I was not rate limited, user query results would show in the output as the test statement does //
-            /*            System.Diagnostics.Debug.WriteLine("Test");*/
         }
 
         public string Title { get; set; }
         public int MaxLength { get; set; }
         public string Text { get; internal set; }
 
-        static async Task RunAsync()
-        {
-            const string apiKey = "AIzaSyCyqAm432caVHD6ycUEWTbCNtg4rD_ao8Y";
-            const string cx = "f45637bb92df9b998";
-            const string query = "suits";
-            var request = WebRequest.Create("https://www.googleapis.com/customsearch/v1?key=" + apiKey + "&cx=" + cx + "&q=" + query);
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Stream dataStream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(dataStream);
-            string responseString = reader.ReadToEnd();
-            dynamic jsonData = JsonConvert.DeserializeObject(responseString);
-            Console.WriteLine(jsonData);
+        //static async Task RunAsync()
+        //{
+        //    const string apiKey = "AIzaSyCyqAm432caVHD6ycUEWTbCNtg4rD_ao8Y";
+        //    const string cx = "f45637bb92df9b998";
+        //    const string query = "suits";
+        //    var request = WebRequest.Create("https://www.googleapis.com/customsearch/v1?key=" + apiKey + "&cx=" + cx + "&q=" + query);
+        //    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        //    Stream dataStream = response.GetResponseStream();
+        //    StreamReader reader = new StreamReader(dataStream);
+        //    string responseString = reader.ReadToEnd();
+        //    dynamic jsonData = JsonConvert.DeserializeObject(responseString);
+        //    Console.WriteLine(jsonData);
 
-/*            var results = new List<Result>();
-            foreach (var item in jsonData)
-            {
-                Console.WriteLine(
-                    item.title,
-                    item.link,
-                    item.cacheId);
-            }*/
-        }
+        //    //var results = new List<Result>();
+        //    //foreach (var item in jsonData)
+        //    //{
+        //    //    Console.WriteLine(
+        //    //        item.title,
+        //    //        item.link,
+        //    //        item.cacheId);
+        //    //}
+        //}
 
         public class Result
         {
